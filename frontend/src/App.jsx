7735b74c22559aba725394de4cf2ce32f8476210
faijanbaig <faijanbaig2308@gmail.com>
2024-09-24 18:20:07 +0530

    fix minor  errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import CreateEmployee from "./pages/CreateEmployee"
 import Dashboard from "./pages/Dashboard"
-import EditEmployee from "./pages/EditEmployee"
-import EmployeeList from "./pages/EmployeeList"
 import Login from "./pages/Login"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 const App = () => {
@@ -13,12 +11,9 @@ const App = () => {
       {/* <Route path="/home" element={<Navbar />} /> */}
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/Create-employee" element={<CreateEmployee />} />
-      <Route path="/employee-list" element={<EmployeeList />} />
-      <Route path="/edit-employee/:id" element={<EditEmployee />} />
     </Routes>
   </Router>
     </div>
-    
   )
 }
 
